d65a97ab894c26a04689674e0b20e3eb1590139c
aodhank25 <aodhank25@hotmail.com>
2022-09-30 20:01:59 +0000

    creat const values for JavaScript

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+const playerScore = 0;
+const computerScore = 0;
+
+const playerScoreSpan = document.getElementById("player_score");
+const computerScoreSpan = document.getElementById("computer_score");
+const scoreBoardDiv = document.querySelector(".score_board");
+const resultDiv = document.querySelector(".outcome")
+const rockDiv = document.getElementById("r") 
+const paperDiv = document.getElementById("p") 
+const scissorsDiv = document.getElementById("s") 

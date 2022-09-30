c21f047534599fa1aec7426d5a3a5fedbf73d380
aodhank25 <aodhank25@hotmail.com>
2022-09-30 20:45:24 +0000

    add HTML elements and random computer choice function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+
+// Setting the scores and selecting our HTML elements.
 const playerScore = 0;
 const computerScore = 0;
 
@@ -8,3 +10,45 @@ const resultDiv = document.querySelector(".outcome")
 const rockDiv = document.getElementById("r") 
 const paperDiv = document.getElementById("p") 
 const scissorsDiv = document.getElementById("s") 
+
+/**
+ * Game function 
+ * compares player choice to computer choice
+ * computer choice and player choice
+ */
+
+function getComputerChoice(){
+    const choices =['r','p', 's'];
+   const randomNumber= Math.floor(Math.random() * 3);
+   return choices[randomNumber];
+}
+
+console.log(getComputerChoice())
+
+function game(playerChoice){
+    const computerChoice
+
+}
+
+/**
+ * selectors for the game options 
+ * RPS 
+ */
+
+ function main(){
+    rockDiv.addEventListener('click',function(){
+    game("r");
+    })
+
+    paperDiv.addEventListener('click',function(){
+        game("p");
+    })
+
+    scissorsDiv.addEventListener('click',function(){
+        game("s");
+})
+
+}
+
+
+
